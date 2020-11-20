b58fe912a83a9d95c1a6e6a91a30251b8dc6f36b
milesbroomfield <milesbroomfield@yahoo.com>
2020-11-20 16:53:52 +0000

    First Real Commit

---
+++ GigMe/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  GigMe
+//
+//  Created by Miles Broomfield on 20/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @EnvironmentObject var viewRouter:ViewRouter
+    @State var viewDisplayed:Views = Views.searchView
+    
+    var body: some View {
+        ZStack{
+            VStack{
+                if(viewDisplayed == Views.searchView){
+                    Text("Search View")
+                        .padding()
+                }
+                else{
+                    Text("Should never be here")
+                }
+            }
+        }
+        .onReceive(viewRouter.$view, perform: { view in
+            self.viewDisplayed = view
+        })
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GigMe/Session.swift
@@ -0,0 +1,20 @@
+//
+//  Session.swift
+//  GigMe
+//
+//  Created by Miles Broomfield on 20/11/2020.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+class Session{
+    
+    //singleton -> object accessible from anywhere within the project
+    let session = Session()
+    
+    
+    //ADD api and data retrieval objects here, views will use methods from this class to retrieve data
+    
+}
