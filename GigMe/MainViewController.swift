c3217fb0299f7f088e502ff8011f637d86819095
milesbroomfield <milesbroomfield@yahoo.com>
2020-11-21 14:35:58 +0000

    check commit

---
+++ GigMe/MainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainViewController.swift
+
+//  Created by Miles Broomfield on 21/11/2020.
+//
+
+
+import Foundation
+import UIKit
+import SwiftUI
+
+class ViewRouter:ObservableObject{
+    
+    @Published var view = Views.searchView
+    
+    //Modal
+    
+    @Published var modalPresented = false
+    @Published var modalContent:AnyView = AnyView(EmptyView())
+    
+    func presentModal(content:AnyView){
+        self.modalContent = content
+        self.modalPresented = true
+    }
+    
+    func dismissModal(){
+        self.modalPresented = false
+        self.modalContent = AnyView(EmptyView())
+    }
+}
+
+enum Views{
+    case searchView
+}
