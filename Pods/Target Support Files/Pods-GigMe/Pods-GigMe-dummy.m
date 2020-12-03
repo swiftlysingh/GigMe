1ace427758d8170dd20d779210d9f47ea076075a
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2020-12-03 20:57:38 +0530

    Add gitignore

--- Pods/Target Support Files/Pods-GigMe/Pods-GigMe-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GigMe : NSObject
-@end
-@implementation PodsDummy_Pods_GigMe
-@end
--- Pods/Target Support Files/Pods-GigMeTests/Pods-GigMeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GigMeTests : NSObject
-@end
-@implementation PodsDummy_Pods_GigMeTests
-@end
